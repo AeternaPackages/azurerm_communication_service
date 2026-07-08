0dc7b5fe0edf7ebcaeb2b6c30bb150e4dd376859
Daniel Allen <dan@aeternagroup.net>
2026-07-08 23:38:26 +0100

    Update to schema version v4.80.0

---
+++ main.tf
@@ -0,0 +1,24 @@
+locals {
+  communication_services = { for k1, v1 in var.communication_services : k1 => { data_location = v1.data_location, name = v1.name, resource_group_name = v1.resource_group_name, tags = v1.tags } }
+
+  communication_service_email_domain_associations = merge([
+    for k1, v1 in var.communication_services : {
+      for k2, v2 in coalesce(v1.communication_service_email_domain_associations, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        communication_service_id = module.communication_services.communication_services["${k1}"].id
+      })
+    }
+  ]...)
+}
+
+module "communication_services" {
+  source                 = "git::https://github.com/AeternaModules/azurerm_communication_service.git?ref=v4.80.0"
+  communication_services = local.communication_services
+}
+
+module "communication_service_email_domain_associations" {
+  source                                          = "git::https://github.com/AeternaModules/azurerm_communication_service_email_domain_association.git?ref=v4.80.0"
+  communication_service_email_domain_associations = local.communication_service_email_domain_associations
+  depends_on                                      = [module.communication_services]
+}
+
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "communication_services" {
+  description = <<EOT
+Map of communication_services, attributes below
+Required:
+    - name
+    - resource_group_name
+Optional:
+    - data_location
+    - tags
+Nested communication_service_email_domain_associations (azurerm_communication_service_email_domain_association):
+    Required:
+        - email_service_domain_id
+EOT
+
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    data_location       = optional(string) # Default: "United States"
+    tags                = optional(map(string))
+    communication_service_email_domain_associations = optional(map(object({
+      email_service_domain_id = string
+    })))
+  }))
+
+  validation {
+    condition = alltrue(concat(
+      [for kk in keys(var.communication_services) : !strcontains(kk, "/")],
+      flatten([for k0, v0 in var.communication_services : [for kk in keys(coalesce(v0.communication_service_email_domain_associations, {})) : !strcontains(kk, "/")]])
+    ))
+    error_message = "Map keys in this package must not contain '/': it is used internally as a nesting-key separator, so a key containing it can silently collide two different nested entries into one. Rename the offending key(s)."
+  }
+}
